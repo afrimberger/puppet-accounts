73f97fb4fdb8eb8d58a0ff83d98ab1ada8e556eb
André Frimberger <github@frimberger.de>
2017-01-19 17:19:31 +0100

    Fix error while settings users to absent

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,11 +19,4 @@ class accounts(
   create_resources(group, $groups)
 
   create_resources(accounts::account, $accounts)
-
-  # Remove users marked as absent
-  $absent_users = keys(absents($users))
-  user { $absent_users:
-    ensure     => absent,
-    forcelocal => $forcelocal,
-  }
 }
